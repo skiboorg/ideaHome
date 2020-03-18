a057f8e2bec8d74f25fc0cdafa17b33138a91c7b
ddnnss <ddnnss.i1@gmail.com>
2020-03-18 23:38:46 +0300

    new styles

--- page/urls.py
+++ page/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('manufacturers/', views.manufacturers, name='manufacturers'),
     path('manufacturers/<slug>/', views.manufacturers_cat, name='manufacturers_cat'),
     path('category/<category_slug>/', views.category, name='category'),
+    path('category/<category_slug>/<subcategory_slug>/', views.subcategory, name='subcategory'),
     path('category/<category_slug>/<subcategory_slug>/<item_slug>', views.item_page, name='item_page'),
 
     # path('subcategory/<subcat_slug>/', views.subcategory, name='subcategory'),
--- page/views.py
+++ page/views.py
@@ -25,14 +25,119 @@ def manufacturers(request):
 def manufacturers_cat(request,slug):
     return render(request, 'page/manufacturers.html', locals())
 
+
+
 def category(request, category_slug):
     all_categories = Category.objects.filter(is_active=True)
     category = Category.objects.get(name_slug=category_slug)
-    items = Item.objects.filter(category=category)
+    items_qs = Item.objects.filter(category=category)
+    items = items_qs
     manufactors = category.get_all_manufactors()
+    qs_filtered = False
+    search_res = False
 
-    print(items)
-    print(manufactors)
+    order = request.GET.get('order')
+    filter_manufactor = request.GET.get('manufactor')
+    search_q = request.GET.get('q')
+
+
+    if search_q:
+        items = items_qs.filter(name_lower__contains=search_q.lower())
+        search_res = True
+        param_search = search_q
+
+    if filter_manufactor:
+        if search_res:
+            items = items.filter(manufactor__name_slug=filter_manufactor)
+        else:
+            items = items_qs.filter(manufactor__name_slug=filter_manufactor)
+        qs_filtered = True
+        param_manufactor = filter_manufactor
+
+    if order == 'price_gte':
+        if qs_filtered or search_res:
+            items = items.order_by('-price')
+        else:
+            items = items_qs.order_by('-price')
+        param_order = order
+
+    if order == 'price_lte':
+        if qs_filtered or search_res:
+            items = items.order_by('price')
+        else:
+            items = items_qs.order_by('price')
+        param_order = order
+
+    if order == 'name_az':
+        if qs_filtered or search_res:
+            items = items.order_by('name')
+        else:
+            items = items_qs.order_by('name')
+        param_order = order
+
+    if order == 'name_za':
+        if qs_filtered or search_res:
+            items = items.order_by('-name')
+        else:
+            items = items_qs.order_by('-name')
+        param_order = order
+    return render(request, 'page/category.html', locals())
+
+def subcategory(request, category_slug, subcategory_slug):
+    all_categories = Category.objects.filter(is_active=True)
+    category = Category.objects.get(name_slug=category_slug)
+    subcategory = SubCategory.objects.get(name_slug=subcategory_slug)
+    items_qs = Item.objects.filter(subcategory=subcategory)
+    items = items_qs
+    manufactors = subcategory.manufactor_set.all()
+    qs_filtered = False
+    search_res = False
+
+    order = request.GET.get('order')
+    filter_manufactor = request.GET.get('manufactor')
+    search_q = request.GET.get('q')
+
+    if search_q:
+        items = items_qs.filter(name_lower__contains=search_q.lower())
+        search_res = True
+        param_search = search_q
+
+    if filter_manufactor:
+        if search_res:
+            items = items.filter(manufactor__name_slug=filter_manufactor)
+        else:
+            items = items_qs.filter(manufactor__name_slug=filter_manufactor)
+        qs_filtered = True
+        param_manufactor = filter_manufactor
+
+    if order == 'price_gte':
+        if qs_filtered or search_res:
+            items = items.order_by('-price')
+        else:
+            items = items_qs.order_by('-price')
+        param_order = order
+
+    if order == 'price_lte':
+        if qs_filtered or search_res:
+            items = items.order_by('price')
+        else:
+            items = items_qs.order_by('price')
+        param_order = order
+
+    if order == 'name_az':
+        if qs_filtered or search_res:
+            items = items.order_by('name')
+        else:
+            items = items_qs.order_by('name')
+        param_order = order
+
+    if order == 'name_za':
+        if qs_filtered or search_res:
+            items = items.order_by('-name')
+        else:
+            items = items_qs.order_by('-name')
+        param_order = order
+
     return render(request, 'page/category.html', locals())
 
 def item_page(request,category_slug,subcategory_slug,item_slug):
