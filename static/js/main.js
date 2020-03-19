f61a5d959bf5522454767cfe4a331f3403cdca5b
xxx <sdfds@dsds.tt>
2020-03-19 15:15:43 +0300

    new sections

--- static/js/main.js
+++ static/js/main.js
@@ -23,26 +23,93 @@ Vue.component('header-cart-items', {
     }
 })
 
-var HeaderCart = new Vue({
+
+Vue.component('main-cart-item', {
     delimiters: ['[[', ']]'],
-    el: '#HeaderCart',
+    props: ['item','index'],
+    template :`  <div class="main-cart-item">
+                            <div class="main-cart-item-name">
+                                <img src="https://dummyimage.com/70x80/000/fff" alt=""> <span>[[item.name]]</span>
+                            </div>
+                            <div class="main-cart-item-number">
+                                <div class="item-info-price-quantity">
+                                    <p v-on:click="delQt(index)">-</p><input style="background: transparent; color:#e63d44 " id="quantity" readonly type="number" :value="[[item.num]]" min="1"><p v-on:click="addQt(index)">+</p>
+                                </div>
+                            </div>
+                            <div class="main-cart-item-price">[[item.price]] <i class="fa fa-rub"></i></div>
+                            <div class="main-cart-item-total-price">[[item.num * item.price]] <i class="fa fa-rub"></i></div>
+                            <div class="main-cart-item-action"><span class="cart-delete-btn" v-on:click="userDelete(index)">&#10006;</span></div>
+                        </div>`,
+
+    methods: {
+        userDelete: function (index) {
+            this.$emit('userdelete', index);
+        },
+        delQt: function (index) {
+
+            this.$emit('del_qt', index);
+        },
+        addQt: function (index) {
+            this.$emit('add_qt', index);
+        }
+    }
+})
+
+var app = new Vue({
+    delimiters: ['[[', ']]'],
+    el: '#app',
     data: {
         cartTotal:0,
+        cartNotEmpty : false,
         headerCartItems: [
         ]
     },
+
     methods:{
         remove: function(index){
             this.headerCartItems.splice(index, 1)
         }
+        ,
+        del_qt: function (index) {
+            if (this.headerCartItems[index]['num'] > 1){
+                this.headerCartItems[index]['num'] -= 1
+                this.updateCart()
+            }
+        },
+        add_qt: function (index) {
+           this.headerCartItems[index]['num'] += 1
+             this.updateCart()
+        },
+        updateCart: function () {
+            console.log('update')
+             this.cartTotal = 0
+            for(var item in this.headerCartItems){
+                console.log(this.headerCartItems[item]['price'])
+                this.cartTotal +=  parseInt(this.headerCartItems[item]['price']) * parseInt(this.headerCartItems[item]['num'])
+            }
+        },
+        addItem: function (event) {
+            item_id = event.target.getAttribute('data-id')
+            item_name = event.target.getAttribute('data-name')
+            item_price = event.target.getAttribute('data-price')
+            console.log('add')
+            this.headerCartItems.push({id:item_id,name:item_name,price:item_price,num:1 })
+           event.target.innerText='в клрзине'
+            event.target.setAttribute('disabled','disabled')
+
+
+
+
+        }
     },
     watch: {
         headerCartItems: function (val) {
+            console.log('change')
             this.cartTotal = 0
             for(var item in val){
                 console.log(val[item]['price'])
                 this.cartTotal +=  parseInt(val[item]['price']) * parseInt(val[item]['num'])
             }
         }
-}
+    }
 })
