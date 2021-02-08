9113d753ffeb3b4fa860561df00775b5ba3c394e
skiboorg <d@skib.org>
2021-02-08 19:39:44 +0300

    add blog

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'khimiya_fiz.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ideaHome.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
